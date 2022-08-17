e61e796024ccf9328d8d50b3478b4243d15be8e3
eamon9 <eamon9@icloud.com>
2022-08-17 13:50:16 +0300

    Initial commit

---
+++ src/AccentureBootcamp/Main.java
@@ -0,0 +1,10 @@
+package AccentureBootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Write your code here Group3
+        int a= 10;
+        System.out.println(a);
+    }
+}
