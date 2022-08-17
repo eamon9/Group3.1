2f35b82a2abf54d0b2adde6e7d66eb78be6143f8
eamon9 <eamon9@icloud.com>
2022-08-17 14:39:20 +0300

    First Task completed

--- src/AccentureBootcamp/Main.java
+++ src/AccentureBootcamp/Main.java
@@ -1,11 +1,40 @@
 package AccentureBootcamp;
 
+import java.util.Scanner;
+
 public class Main {
 
+    //1) Ask user to input two numbers, compare them and output result:
+    //   Input:
+    //       Enter value a: 10
+    //       Enter value b: 10
+    //   Expected output:
+    //       is 10 equal to 10? - true
+    //       is 10 less than 10? - false
+    //       is 10 less or equal to 10? - true
+    //       is 10 greater than 10? - false
+    //       is 10 greater or equal to 10? - true
+
     public static void main(String[] args) {
-	//Write your code here Group3
-        int a= 10;
-        System.out.println(a);
+	Scanner in= new Scanner(System.in);
+    int a, b;
+    System.out.println("Enter value a: ");
+    a= in.nextInt();
+    System.out.println("Enter value b: ");
+    b= in.nextInt();
+    System.out.println(MathTasks(a, b));
 
     }
+
+    public static String MathTasks(int a, int b) {
+        String isEqual= "is "+a+" equal to "+b+"? -"+(a==b)+"\n";
+        String isLessThan= "is "+a+" less than "+b+"? -"+(a<b)+"\n";
+        String isLessOrEqual= "is "+a+" less or equal to "+b+"? -"+(a<=b)+"\n";
+        String isGreater= "is "+a+" greater than "+b+"? -"+(a>b)+"\n";
+        String isGreaterOrEqual= "is "+a+" greater or equal to "+b+"? -"+(a>=b);
+        return isEqual+isLessThan+isLessOrEqual+isGreater+isGreaterOrEqual;
+    }
+
+
+
 }
