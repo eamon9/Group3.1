3c1beb1c6812435059642112dffd27929b5bc748
eamon9 <eamon9@icloud.com>
2022-08-17 14:55:19 +0300

    Second Task completed

---
+++ src/AccentureBootcamp/Day2Tasks/Task1.java
@@ -0,0 +1,37 @@
+package AccentureBootcamp.Day2Tasks;
+
+import java.util.Scanner;
+
+/*
+1) Ask user to input two numbers, compare them and output result:
+   Input:
+       Enter value a: 10
+       Enter value b: 10
+   Expected output:
+       is 10 equal to 10? - true
+       is 10 less than 10? - false
+       is 10 less or equal to 10? - true
+       is 10 greater than 10? - false
+       is 10 greater or equal to 10? - true
+*/
+
+public class Task1 {
+    public Task1() {
+        Scanner in= new Scanner(System.in);
+        int a, b;
+        System.out.println("Enter value a: ");
+        a= in.nextInt();
+        System.out.println("Enter value b: ");
+        b= in.nextInt();
+        System.out.println(MathTasks(a, b));
+    }
+
+    public static String MathTasks(int a, int b) {
+        String isEqual= "is "+a+" equal to "+b+"? -"+(a==b)+"\n";
+        String isLessThan= "is "+a+" less than "+b+"? -"+(a<b)+"\n";
+        String isLessOrEqual= "is "+a+" less or equal to "+b+"? -"+(a<=b)+"\n";
+        String isGreater= "is "+a+" greater than "+b+"? -"+(a>b)+"\n";
+        String isGreaterOrEqual= "is "+a+" greater or equal to "+b+"? -"+(a>=b);
+        return isEqual+isLessThan+isLessOrEqual+isGreater+isGreaterOrEqual;
+    }
+}
---
+++ src/AccentureBootcamp/Day2Tasks/Task2.java
@@ -0,0 +1,23 @@
+package AccentureBootcamp.Day2Tasks;
+
+import java.util.Scanner;
+
+/*
+    2) Ask user to input two boolean values and compare them.
+    Input:
+       Enter b1: true
+       Enter b2: false
+    Expected output:
+       Is b1 and b2 equal? - false
+*/
+public class Task2 {
+    public Task2() {
+        boolean b1, b2;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter b1: ");
+        b1= in.nextBoolean();
+        System.out.println("Enter b2: ");
+        b2= in.nextBoolean();
+        System.out.println("Is "+b1+" and "+b2+" equal? -"+(b1==b2));
+    }
+}
