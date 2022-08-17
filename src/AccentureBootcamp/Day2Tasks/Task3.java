d92d1ed560b91a580687490add8849a265fe0cc1
eamon9 <eamon9@icloud.com>
2022-08-17 15:02:03 +0300

    Third Task completed

---
+++ src/AccentureBootcamp/Day2Tasks/Task3.java
@@ -0,0 +1,21 @@
+package AccentureBootcamp.Day2Tasks;
+
+import java.util.Scanner;
+/*
+3) Ask user to input a whole number and output true, if it is even number and false if it is odd number
+    Hint: use remainder operator to divide number by two and get reminder (if reminder is 0 then it means it is even number)-
+    */
+
+public class Task3 {
+    public Task3() {
+        int userNum;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Input a whole number: ");
+        userNum= in.nextInt();
+        if(userNum%2== 0) {
+            System.out.println("Your number is even");
+        } else{
+            System.out.println("Your number is odd");
+        }
+    }
+}
--- src/AccentureBootcamp/Main.java
+++ src/AccentureBootcamp/Main.java
@@ -2,10 +2,12 @@ package AccentureBootcamp;
 
 import AccentureBootcamp.Day2Tasks.Task1;
 import AccentureBootcamp.Day2Tasks.Task2;
+import AccentureBootcamp.Day2Tasks.Task3;
 
 public class Main {
     public static void main(String[] args) {
         //new Task1();
-        new Task2();
+        //new Task2();
+        new Task3();
     }
 }
