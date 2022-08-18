27fc590cdb5dd99fbb9fe724cee19a565632fe58
eamon9 <eamon9@icloud.com>
2022-08-18 11:00:03 +0300

    2nd Task completed

--- src/AccentureBootcamp/Day3Tasks/Task3_2.java
+++ src/AccentureBootcamp/Day3Tasks/Task3_2.java
@@ -5,8 +5,28 @@ package AccentureBootcamp.Day3Tasks;
 // Write a Java program to copy this array by iterating it.
 */
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task3_2 {
     public Task3_2() {
+        Random r= new Random();
+        int[] arbitraryNums= new int[]{r.nextInt(100), r.nextInt(100), r.nextInt(100), r.nextInt(100)};
+        System.out.println("default: "+Arrays.toString(arbitraryNums));
 
+        //for loop to copy each number
+        int[] copiedArray= new int[arbitraryNums.length];
+        for (int i = 0; i < arbitraryNums.length; i++) {
+            copiedArray[i]= arbitraryNums[i];
+        }
+        System.out.println("with for loop: "+Arrays.toString(copiedArray));
+
+        //or you can just use copy array method
+        int[] copiedArray2= Arrays.copyOf(arbitraryNums, arbitraryNums.length);
+        System.out.println("with method: "+Arrays.toString(copiedArray2));
     }
+
+
+
+
 }
